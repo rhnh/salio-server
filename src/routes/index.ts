7ae027f390a1f0b362f245d5ab470974e0d822b0
rhnh <nrhbd@protonmail.com>
2021-04-25 12:37:23 +0200

    app router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,10 @@
-export { listRouter } from "./list-routes";
-export { taxonomyRouter } from "./taxonomy-routes";
-export { userRouter } from "./user-routes";
+import { Router } from "express";
+import { listRouter } from "./list-routes";
+import { taxonomyRouter } from "./taxonomy-routes";
+import { userRouter } from "./user-routes";
+
+export const AppRouter = Router();
+
+AppRouter.use("/lists", listRouter);
+AppRouter.use("/items", taxonomyRouter);
+AppRouter.use("/users", userRouter);
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { asyncFn } from "../utils/helpers";
-import { isGuest, registerUser } from "./user-controllers";
+import { registerUser } from "./user-controllers";
 import { body } from "express-validator";
+import passport from "passport";
 
 export const userRouter = Router();
 
@@ -9,12 +10,15 @@ userRouter.post(
   "/new",
   body("username").not().isEmpty().trim().isLength({ min: 3 }),
   body("password").not().isEmpty().trim().isLength({ min: 3 }),
-  asyncFn(isGuest),
   asyncFn(registerUser)
 );
 
 userRouter.post(
   "/login",
   body("username").not().isEmpty().trim().isLength({ min: 3 }),
-  body("password").not().isEmpty().trim().isLength({ min: 3 })
+  body("password").not().isEmpty().trim().isLength({ min: 3 }),
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    successRedirect: "/",
+  })
 );
