dffd4044810b23699d11ad102417b1447d422a24
rhnh <nrhbd@protonmail.com>
2021-04-25 17:00:01 +0200

    Fix: login bug

--- src/utils/user-manager.ts
+++ src/utils/user-manager.ts
@@ -1,28 +1,41 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
-import { findUser, findUserBy } from "../models/user-models";
+import { findUserById, findUserByUsername } from "../models/user-models";
 import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
 import { IUser } from "../types";
+import { isVerifiedPass } from "./password";
 
 export const authentication = (): void => {
   passport.use(
     new LocalStrategy(async (username, password, done) => {
       try {
-        const user: IUser | null = await findUser({ username, password });
-        return user !== null ? done(null, user) : done(null, false);
+        const user: IUser | null = await findUserByUsername(username);
+        if (user && isVerifiedPass(password, user?.password)) {
+          return user !== null ? done(null, user) : done(null, false);
+        }
+        return done(null, false);
       } catch (error) {
         return done(error, false);
       }
     })
   );
   //needs to save user._id
-  passport.serializeUser((user, done) => {
-    done(null, user);
+  passport.serializeUser(async (user, done) => {
+    console.log(user);
+    try {
+      return user !== null ? done(null, user) : done(null, false);
+    } catch (error) {
+      return done(error, false);
+    }
   });
 
   passport.deserializeUser(async (id: string, done) => {
-    const user = await findUserBy(id);
-    done(null, user);
+    try {
+      const user = await findUserById(id);
+      return user !== null ? done(null, user) : done(null, false);
+    } catch (error) {
+      return done(error, false);
+    }
   });
 };
 
@@ -36,7 +49,7 @@ export const authorization = (): void => {
       },
       async (payload, done) => {
         try {
-          const user = findUserBy(payload.sub);
+          const user = findUserById(payload.sub);
           return user !== null ? done(user, null) : done(null, false);
         } catch (error) {
           done(error, false);
@@ -45,3 +58,7 @@ export const authorization = (): void => {
     )
   );
 };
+
+export const verifyUser = passport.authenticate("jwt", {
+  session: false,
+});
