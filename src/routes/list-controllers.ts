ff74278506fbf89f425f6faf05c93dbc406423ef
rhnh <nrhbd@protonmail.com>
2021-04-26 12:59:29 +0200

    delete list

--- src/routes/list-controllers.ts
+++ src/routes/list-controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
-import { addList } from '../models/list-models'
+import { addList, deleteList } from '../models/list-models'
 import { httpStatus, IUser } from '../types'
+
 export async function addListCtrl(
   req: Request,
   res: Response
@@ -18,6 +19,39 @@ export async function addListCtrl(
     }
     return res.status(httpStatus.error).json({
       message: `Something went wrong`,
+      done: false,
+    })
+  } catch (error) {
+    return res.json({
+      status: httpStatus.badRequest,
+      done: false,
+    })
+  }
+}
+
+export async function deleteListCtrl(
+  req: Request,
+  res: Response
+): Promise<Response | void> {
+  try {
+    const { listName } = req.params
+
+    const { username } = req.user as IUser
+    if (listName === '' || !listName) {
+      return res
+        .status(httpStatus.badRequest)
+        .json({ message: 'Something went wrong', done: true })
+    }
+    const isDeleted = await deleteList({ username, listName })
+    if (isDeleted) {
+      return res.json({
+        message: `Successfully deleted ListName: "${listName}"`,
+        done: true,
+        user: req.user,
+      })
+    }
+    return res.status(httpStatus.error).json({
+      message: `Something went wrong,No ListName: "${listName}" found`,
       done: false,
     })
   } catch (error) {
--- src/routes/list-routes.ts
+++ src/routes/list-routes.ts
@@ -1,14 +1,23 @@
-import { Router } from "express";
-import { addListCtrl } from "./list-controllers";
-import { body } from "express-validator";
-import { verifyUser } from "../utils/user-manager";
+import { Router } from 'express'
+import { addListCtrl, deleteListCtrl } from './list-controllers'
+import { body } from 'express-validator'
+import { verifyUser } from '../utils/user-manager'
+import { asyncFn } from '../utils/helpers'
 
-export const listRouter = Router();
+export const listRouter = Router()
 
 listRouter.post(
-  "/new",
-  body("username").not().isEmpty().trim().isLength({ min: 3 }),
-  body("listName").not().isEmpty().trim().isLength({ min: 3 }),
+  '/new',
+  body('username').not().isEmpty().trim().isLength({ min: 3 }),
+  body('listName').not().isEmpty().trim().isLength({ min: 3 }),
   verifyUser,
-  addListCtrl
-);
+  asyncFn(addListCtrl)
+)
+
+listRouter.delete(
+  '/:listName',
+  body('username').not().isEmpty().trim().isLength({ min: 3 }),
+  body('listName').not().isEmpty().trim().isLength({ min: 3 }),
+  verifyUser,
+  asyncFn(deleteListCtrl)
+)
